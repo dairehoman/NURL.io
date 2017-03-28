d685f6bd208879be0869106e96c87fddbf6b24df
dairehoman <dairehoman@gmail.com>
2017-03-28 11:51:22 +0100

    security works, no auth yet

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,19 +1,28 @@
 <?php
 
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
-class DefaultController extends Controller {
-
+use Symfony\Component\HttpFoundation\Session\Session;
+class DefaultController extends Controller
+{
     /**
      * @Route("/", name="index")
      */
-    public function indexAction(Request $request) {
-        $argsArray = [ ];
-
+    public function indexAction(Request $request)
+    {
+        $argsArray =  [ ];
         $templateName = 'index';
-        return $this->render($templateName . '.html.twig', $argsArray); }
+        return $this->render($templateName . '.html.twig', $argsArray);
+    }
+    /**
+     * @Route("/clear_session", name="clear_session")
+     */
+    public function clearSessionAction(Request $request)
+    {
+        $session = new Session();
+        $session->clear();
+        return $this->redirectToRoute('index');
+    }
 }
